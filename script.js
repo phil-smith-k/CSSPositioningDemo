98690930739d13b3ceaeab35c75cb6e99f9a414f
Philip Smith <philip.smith.k@gmail.com>
2020-04-29 15:50:06 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+
+const rect1 = box1.getBoundingClientRect();
+const rect2 = box2.getBoundingClientRect();
+const rect3 = box3.getBoundingClientRect();
+const rect4 = box4.getBoundingClientRect();
